db4ba75dbc228d08da57d74c6c96879f8d2d1982
Axinella Zehi <axinellazehi@gmail.com>
2023-12-03 00:50:56 +0100

    ANIMATION TITRE TROUVER FONCTIONNEL H2 SECTION STORY A METTRE SUR LES AUTRES TITRES - ANIMATION NUAGE OK

--- wp-content/themes/foce-child/front-page.php
+++ wp-content/themes/foce-child/front-page.php
@@ -11,7 +11,10 @@ get_header();
             </video>
         </section>
         <section id="#story" class="story">
-            <h2>L'histoire</h2>
+            <h2 class="title-h2"> 
+                <span class="fadeinBottom" >L'</span>
+                <span class="fadeinBottom-2">histoire</span>
+            </h2>
             <article id="" class="story__article">
                 <p><?php echo get_theme_mod('story'); ?></p>
             </article>
@@ -36,8 +39,8 @@ get_header();
                     <h3>Le Lieu</h3>
                     <p><?php echo get_theme_mod('place'); ?></p>
                     <div  class="slider__inner--place">
-                        <img  class="cloud big " src=" <?php echo get_stylesheet_directory_uri() . './images_koukaki/big_cloud.png'; ?>">
-                        <img class="cloud little" src="<?php echo get_stylesheet_directory_uri() . './images_koukaki/little_cloud.png'; ?> ">
+                        <img  data-bottom-top="transform:translate3d(-270px, 0, 0)" data-top-bottom="transform:translate3d(300px, 0, 0)"  class="cloud big " src=" <?php echo get_stylesheet_directory_uri() . './images_koukaki/big_cloud.png'; ?>">
+                        <img data-bottom-top="transform:translate3d(15px, 0, 0)" data-top-bottom="transform:translate3d(300px, 0, 0)" class="cloud little" src="<?php echo get_stylesheet_directory_uri() . './images_koukaki/little_cloud.png'; ?> ">
                         
                     </div>
                 </div>
@@ -47,7 +50,10 @@ get_header();
 
 
         <section id="studio">
-            <h2>Studio Koukaki</h2>
+            <h2 class="title-h2">
+                <span class="fadeinBottom">Studio</span>
+                <span class="fadeinBottom-2">Koukaki</span>
+            </h2>
             <div>
                 <p>Acteur majeur de l’animation, Koukaki est un studio intégré fondé en 2012 qui créé, produit et distribue des programmes originaux dans plus de 190 pays pour les enfants et les adultes. Nous avons deux sections en activité : le long métrage et le court métrage. Nous développons des films fantastiques, principalement autour de la culture de notre pays natal, le Japon.</p>
                 <p>Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
--- wp-content/themes/foce-child/js/script.js
+++ wp-content/themes/foce-child/js/script.js
@@ -9,7 +9,7 @@ let observer = new IntersectionObserver((entries, observer) => {
 }); 
 
 
-observer.observe(document.querySelector('.story'));
+//observer.observe(document.querySelector('.story'));
 observer.observe(document.querySelector('#studio'));
 //observer.observe(document.querySelector('.banner img'));
 observer.observe(document.querySelector('#characters'));
@@ -53,3 +53,32 @@ var swiper = new Swiper(".mySwiper", {
         el: ".swiper-pagination",
       },
     });
+
+// A  n i m a t i o n  :  T i t r e s 
+AOS.init({
+    duration: 2200
+});
+
+
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.fadeinBottom').each( function(i){
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1','top':'2%'},2000);
+            }
+        });
+    });
+
+    $(window).scroll( function(){
+        $('.fadeinBottom-2').each( function(i){
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1','top':'2%'},2500);
+            }
+        });
+    });
+});
+
